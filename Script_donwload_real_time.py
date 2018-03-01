38145d070db1463e4ed741b0ec95d05f61f6a5e6
sa7sa7 <salah.rekik@gmail.com>
2018-03-01 20:28:43 +0100

    edit Chart position

--- Script_donwload_real_time.py
+++ Script_donwload_real_time.py
@@ -1,7 +1,9 @@
 import urllib3
 from time import sleep
 http = urllib3.PoolManager()
-i = 106;
+i = 0
+if(i > 296):
+    i = 0;
 while(1):
     r = http.request('GET', 'https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json')
     i = i +1 
